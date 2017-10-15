421f20e25eaa1a0924b8f0a59c73ed350073e601
Shawn Baker <sjbaker47@gmail.com>
2017-10-15 13:43:37 -0400

    Minor comments

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,10 @@ use protocol::{DnsQuery, DnsResponse};
 
 fn main() {
     let hostname = env::args().nth(1).expect("Usage: nslookup <hostname>");
+    //XXX: Randomize port? Try binding to a different port if it fails?
     let socket = UdpSocket::bind("0.0.0.0:29341").unwrap();
     let request = DnsQuery::addr_query(hostname);
+    //XXX: Custom DNS server, get default from host?
     request.send_to(&socket, "8.8.8.8:53");
 
     socket.set_read_timeout(Some(Duration::new(5, 0))).expect("Failed to set socket duration!");
